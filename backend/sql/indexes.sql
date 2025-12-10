1264074bf061e5f257fc2194a96c5233a7127fed
vaskozlov <vaskozlov3940@gmail.com>
2025-12-10 12:47:56 +0300

    feat: indexes

---
+++ backend/sql/indexes.sql
@@ -0,0 +1,12 @@
+CREATE INDEX IF NOT EXISTS idx_users_email
+    ON application_users (email);
+
+CREATE INDEX IF NOT EXISTS idx_users_username
+    ON application_users (username);
+
+CREATE INDEX IF NOT EXISTS idx_event_status
+    ON event (status);
+
+CREATE INDEX IF NOT EXISTS idx_outcome_event_id
+    ON outcome (event_id);
+
