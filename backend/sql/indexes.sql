f66fdab40b402cb34d6cb2a10d4f442cb7e6d7d5
vaskozlov <vaskozlov3940@gmail.com>
2025-12-11 11:40:34 +0300

    fix: index for event id

--- backend/sql/indexes.sql
+++ backend/sql/indexes.sql
@@ -4,6 +4,9 @@ CREATE INDEX IF NOT EXISTS idx_users_email
 CREATE INDEX IF NOT EXISTS idx_users_username
     ON application_users (username);
 
+CREATE INDEX IF NOT EXISTS idx_event_id
+    ON event (id);
+
 CREATE INDEX IF NOT EXISTS idx_event_status
     ON event (status);
 
